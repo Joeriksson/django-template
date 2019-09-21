8b8b6c053fd2ba4d8ab25b1035722aae76177adb
J-O Eriksson <joeriksson@hemdatorn.se>
2019-09-21 17:12:52 +0200

    work on the users app

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # django admin
     path('admin/', admin.site.urls),
+    # user management
+    path('accounts/', include('django.contrib.auth.urls')),
+    # local apps
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls'))
 ]
