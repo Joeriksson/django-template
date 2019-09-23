4d1336281f8f229497d562d2d8c496dcc034e91e
J-O Eriksson <joeriksson@hemdatorn.se>
2019-09-23 07:57:26 +0200

    add staticfiles settings and mod base.html

--- project/settings.py
+++ project/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # custom settings
 AUTH_USER_MODEL = 'users.CustomUser'
