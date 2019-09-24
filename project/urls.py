25add0cfbab40feff5672b4c6911bdd2c2b65b44
Jan-Olov Eriksson <janeri@C02YT1VSLVCH.local>
2019-09-24 19:19:31 +0200

    add user profile page

--- project/urls.py
+++ project/urls.py
@@ -3,11 +3,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from . import views
+
 urlpatterns = [
                   # django admin
                   path('admin/', admin.site.urls),
                   # user management
                   path('accounts/', include('allauth.urls')),
+                  path('user_profile/<int:pk>', views.UserUpdate.as_view(), name='user_edit'),
                   # local apps
                   path('', include('pages.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
