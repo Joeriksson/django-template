e201135db223a876410de6a95ad64c661a176b81
J-O Eriksson <25037079+Joeriksson@users.noreply.github.com>
2024-06-03 17:44:04 +0200

    make changes to work with new django-allauth version

--- core/settings/base.py
+++ core/settings/base.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
